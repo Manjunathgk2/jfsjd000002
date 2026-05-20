42ed564c657b37f93b2b189154b8d222a93d1d2c
mgk <manjunathgk16198@gmail.com>
2026-05-20 11:11:14 +0530

    2 nd version

--- java8.java
+++ java8.java
@@ -4,8 +4,8 @@ import java.util.stream.Stream;
 
 public class java8 {
     public static void main(String[] args) {
-      List<String> names= Arrays.asList("Ajay","Ram","Abhi","Mgk","Bharath");
-      Stream<String> s1=names.stream().filter(name -> name.startsWith("A")).map(name->name.toUpperCase());
+      List<String> data= Arrays.asList("Ajay","Ram","Abhi","Mgk","Bharath");
+      Stream<String> s1=data.stream().filter(name -> name.startsWith("A")).map(name->name.toUpperCase());
       s1.forEach(System.out::println);
     }
 }
